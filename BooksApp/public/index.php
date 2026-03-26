095478f9864ec02217e50a4d31564b130f9e5dd7
Eva <eva.jezkova@tul.cz>
2026-03-26 11:15:54 +0100

    #8 - index.php

---
+++ BooksApp/public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Pro účely výuky a laděn na lokálním serveru (např. xampp)
+//je vhodné zapnout kompletní zobrazování chyb
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Načtení třídy routeru, která se postará o zpracování URL
+require_once '../core/App.php';
+
+//Inicializace aplikace a spuštění procesu routování
+$ = new App();
+
